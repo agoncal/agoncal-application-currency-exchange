e0c0445c0f321393bff9b057560f8428c91a9528
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-09-24 17:06:29 +0200

    Cards

--- portfolio/src/main/java/org/agoncal/application/currencyexchange/portfolio/PortfolioService.java
+++ portfolio/src/main/java/org/agoncal/application/currencyexchange/portfolio/PortfolioService.java
@@ -20,7 +20,7 @@ public class PortfolioService {
         "john.doe@example.com", List.of(
             new Portfolio(2L, USER1, "EUR", BigDecimal.valueOf(850.0), LocalDateTime.now()),
             new Portfolio(3L, USER1, "GBP", BigDecimal.valueOf(750.0), LocalDateTime.now()),
-            new Portfolio(4L, USER1, "JPY", BigDecimal.valueOf(100000.0), LocalDateTime.now()),
+            new Portfolio(4L, USER1, "JPY", BigDecimal.valueOf(1000.0), LocalDateTime.now()),
             new Portfolio(5L, USER1, "CHF", BigDecimal.valueOf(900.0), LocalDateTime.now()),
             new Portfolio(6L, USER1, "CAD", BigDecimal.valueOf(1200.0), LocalDateTime.now()),
             new Portfolio(7L, USER1, "AUD", BigDecimal.valueOf(1100.0), LocalDateTime.now())
@@ -28,13 +28,13 @@ public class PortfolioService {
         "jane.smith@example.com", List.of(
             new Portfolio(9L, USER2, "EUR", BigDecimal.valueOf(1700.0), LocalDateTime.now()),
             new Portfolio(10L, USER2, "GBP", BigDecimal.valueOf(1500.0), LocalDateTime.now()),
-            new Portfolio(11L, USER2, "JPY", BigDecimal.valueOf(200000.0), LocalDateTime.now()),
+            new Portfolio(11L, USER2, "JPY", BigDecimal.valueOf(2000.0), LocalDateTime.now()),
             new Portfolio(12L, USER2, "CHF", BigDecimal.valueOf(1800.0), LocalDateTime.now())
         ),
         "bob.johnson@example.com", List.of(
             new Portfolio(16L, USER3, "EUR", BigDecimal.valueOf(425.0), LocalDateTime.now()),
             new Portfolio(17L, USER3, "GBP", BigDecimal.valueOf(375.0), LocalDateTime.now()),
-            new Portfolio(18L, USER3, "JPY", BigDecimal.valueOf(50000.0), LocalDateTime.now()),
+            new Portfolio(18L, USER3, "JPY", BigDecimal.valueOf(500.0), LocalDateTime.now()),
             new Portfolio(19L, USER3, "CHF", BigDecimal.valueOf(450.0), LocalDateTime.now()),
             new Portfolio(21L, USER3, "AUD", BigDecimal.valueOf(550.0), LocalDateTime.now())
         )
