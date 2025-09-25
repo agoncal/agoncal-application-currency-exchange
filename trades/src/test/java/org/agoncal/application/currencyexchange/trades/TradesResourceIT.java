497f25862474bdf85ac11f60cca430f67dbe88dd
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-09-25 15:56:06 +0200

    Adding gRPC

--- trades/src/test/java/org/agoncal/application/currencyexchange/trades/TradesResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.agoncal.application.currencyexchange.trades;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class TradesResourceIT extends TradesResourceTest {
-    // Execute the same tests but in packaged mode.
-}
