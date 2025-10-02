231b907ffe1e1c426ff4fba44b4c52c379cbda69
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-10-02 15:10:33 +0200

    Fallback

--- portfolio/src/main/java/org/agoncal/application/currencyexchange/portfolio/PortfolioService.java
+++ portfolio/src/main/java/org/agoncal/application/currencyexchange/portfolio/PortfolioService.java
@@ -9,11 +9,13 @@ import org.agoncal.application.currencyexchange.currency.ExchangeRateServiceGrpc
 import static org.agoncal.application.currencyexchange.portfolio.User.USER_PORTFOLIOS;
 import org.agoncal.application.currencyexchange.portfolio.trade.Trade;
 import org.agoncal.application.currencyexchange.portfolio.trade.TradeService;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -29,24 +31,29 @@ public class PortfolioService {
     TradeService tradeService;
 
     public List<Portfolio> getUserPortfolio(String userId) {
+        LOG.info("Get portfolio for user " + userId);
+
         return USER_PORTFOLIOS.getOrDefault(userId, List.of())
             .stream()
             .sorted(Comparator.comparing(Portfolio::currency))
             .toList();
     }
 
+    @Fallback(fallbackMethod = "fallbackGetAllCurrentRates")
     public List<ExchangeRate> getAllCurrentRates() {
         LOG.info("Get all currency rates");
-        
+
         return exchangeRateService.getAllCurrentRates(Empty.newBuilder().build()).getRatesList();
     }
 
+    @Fallback(fallbackMethod = "fallbackGetCurrentRate")
     public ExchangeRate getCurrentRate(String currencyCode) {
         LOG.info("Get currency rate: " + currencyCode);
 
         return exchangeRateService.getCurrentRate(CurrencyRequest.newBuilder().setCurrencyCode(currencyCode).build()).getRate();
     }
 
+    @Fallback(fallbackMethod = "fallbackExecuteTrade")
     public void executeTrade(Trade trade) {
         LOG.info("Execute trade: " + trade);
 
@@ -54,10 +61,43 @@ public class PortfolioService {
         updateUserPortfolio(trade);
     }
 
+    @Fallback(fallbackMethod = "fallbackGetAllTrades")
     public List<Trade> getAllTrades(String userId) {
         LOG.info("Get all trades");
 
         return tradeService.getAllTrades(userId);
+    }
+
+    public List<ExchangeRate> fallbackGetAllCurrentRates() {
+        LOG.warn("Falling back on get all currency rates");
+        return List.of(
+            ExchangeRate.newBuilder().setCurrencyCode("AUD").setRate(0).build(),
+            ExchangeRate.newBuilder().setCurrencyCode("CAD").setRate(0).build(),
+            ExchangeRate.newBuilder().setCurrencyCode("CHF").setRate(0).build(),
+            ExchangeRate.newBuilder().setCurrencyCode("EUR").setRate(0).build(),
+            ExchangeRate.newBuilder().setCurrencyCode("GBP").setRate(0).build(),
+            ExchangeRate.newBuilder().setCurrencyCode("JPY").setRate(0).build()
+        );
+    }
+
+    public ExchangeRate fallbackGetCurrentRate(String currencyCode) {
+        LOG.warn("Falling back on get currency rate: " + currencyCode);
+
+        return ExchangeRate.newBuilder().setCurrencyCode(currencyCode).setRate(0).build();
+    }
+
+    private static final List<Trade> FALLBACK_TRADES = new ArrayList<>();
+
+    public void fallbackExecuteTrade(Trade trade) {
+        LOG.warn("Falling back on execute trade: " + trade);
+
+        FALLBACK_TRADES.add(trade);
+    }
+
+    public List<Trade> fallbackGetAllTrades(String userId) {
+        LOG.warn("Falling back on get all trades");
+
+        return FALLBACK_TRADES;
     }
 
     private static void updateUserPortfolio(Trade trade) {
